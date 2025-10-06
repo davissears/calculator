a0bd62fe2ea731dcbc2b8b3cb6e6cb61264852ce
davissears <davisrandsears@gmail.com>
2025-10-06 10:09:18 -0700

    styling: Minor tweaks
    also:
    -uncommented declared display variables in events.js

--- events.js
+++ events.js
@@ -3,6 +3,6 @@
 import { add, subtract, multiply, divide, operate } from "./script.js";
 
 //* display variables
-// let operandL;
-// let operandR;
-// let operator;
+let operandL;
+let operandR;
+let operator;
