bf23efd57f4e4bf2c28727d58ef21bcd484dac41
davissears <davisrandsears@gmail.com>
2025-10-10 17:53:49 -0700

    unified formatting:
    switched to using biome

--- events.js
+++ events.js
@@ -1,6 +1,5 @@
 //* Module Imports
 
-import { add, subtract, multiply, divide, operate } from "./script.js";
 //reference for entire form
 const calculatorForm = document.getElementById("calculator-Form");
 //* display variables
@@ -22,50 +21,50 @@ let currentState = "leftOperand";
 
 //* button Events
 calculatorForm.addEventListener("click", (e) => {
-  e.preventDefault();
-  const button = e.target;
-  const displayContent = button.value;
-  //* display state management
-  if (button.className === "button") {
-    if (currentState === "leftOperand") {
-      operandDisplayL.append(displayContent);
-    } else if (currentState === "rightOperand") {
-      operandDisplayR.append(displayContent);
-    }
-    //* operator button event handling
-  } else if (button.className === "operator-Button") {
-    if (operandDisplayL.textContent !== "") {
-      currentState = "rightOperand";
-      operatorDisplay.textContent = displayContent;
-      // getOperation();
-    }
-    //* clear button state management
-  } else if (button.className === "clearButton") {
-    operandDisplayL.textContent = "";
-    operatorDisplay.textContent = "";
-    operandDisplayR.textContent = "";
-    resultDisplay.textContent = "";
-    getOperation();
-    currentState = "leftOperand";
-  }
+	e.preventDefault();
+	const button = e.target;
+	const displayContent = button.value;
+	//* display state management
+	if (button.className === "button") {
+		if (currentState === "leftOperand") {
+			operandDisplayL.append(displayContent);
+		} else if (currentState === "rightOperand") {
+			operandDisplayR.append(displayContent);
+		}
+		//* operator button event handling
+	} else if (button.className === "operator-Button") {
+		if (operandDisplayL.textContent !== "") {
+			currentState = "rightOperand";
+			operatorDisplay.textContent = displayContent;
+			// getOperation();
+		}
+		//* clear button state management
+	} else if (button.className === "clearButton") {
+		operandDisplayL.textContent = "";
+		operatorDisplay.textContent = "";
+		operandDisplayR.textContent = "";
+		resultDisplay.textContent = "";
+		getOperation();
+		currentState = "leftOperand";
+	}
 });
 //* Call & display Operate
 function getOperation() {
-  let a = operandDisplayL.textContent;
-  console.log(a);
-  let o = operatorDisplay.textContent;
-  console.log(o);
-  let b = operandDisplayR.value;
-  console.log(b);
-  return { a: a, o: o, b: b };
+	const a = operandDisplayL.textContent;
+	console.log(a);
+	const o = operatorDisplay.textContent;
+	console.log(o);
+	const b = operandDisplayR.value;
+	console.log(b);
+	return { a: a, o: o, b: b };
 }
 
 const equals = document.getElementById("equals-Button");
-equals.addEventListener("click", (e) => {
-  const opVar = getOperation();
+equals.addEventListener("click", () => {
+	const opVar = getOperation();
 
-  operate(opVar, opVar.a, opVar.o, opVar.b);
-  console.log(operationResult);
+	operate(opVar, opVar.a, opVar.o, opVar.b);
+	console.log(operationResult);
 
-  resultDisplay.textContent = operate(opVar, opVar.a, opVar.o, opVar.b);
+	resultDisplay.textContent = operate(opVar, opVar.a, opVar.o, opVar.b);
 });
--- script.js
+++ script.js
@@ -1,38 +1,38 @@
 //* Operator Functions
 let operationResult;
 export function add(a, b) {
-  operationResult = a + b;
-  return operationResult;
+	operationResult = a + b;
+	return operationResult;
 }
 
 export function subtract(a, b) {
-  operationResult = a - b;
-  return operationResult;
+	operationResult = a - b;
+	return operationResult;
 }
 export function multiply(a, b) {
-  operationResult = a * b;
-  return operationResult;
+	operationResult = a * b;
+	return operationResult;
 }
 
 export function divide(a, b) {
-  if (b === 0) {
-    operationResult = "Error: Cannot divide by zero";
-  } else {
-    operationResult = a / b;
-  }
-  return operationResult;
+	if (b === 0) {
+		operationResult = "Error: Cannot divide by zero";
+	} else {
+		operationResult = a / b;
+	}
+	return operationResult;
 }
 
 export function operate(o, a, b) {
-  if (o === `+`) {
-    return add(a, b);
-  } else if (o === `-`) {
-    return subtract(a, b);
-  } else if (o === `/`) {
-    return divide(a, b);
-  } else if (o === `*`) {
-    return multiply(a, b);
-  } else {
-    return "Error: Invalid operator";
-  }
+	if (o === `+`) {
+		return add(a, b);
+	} else if (o === `-`) {
+		return subtract(a, b);
+	} else if (o === `/`) {
+		return divide(a, b);
+	} else if (o === `*`) {
+		return multiply(a, b);
+	} else {
+		return "Error: Invalid operator";
+	}
 }
